7ffd68c3453bb7c45587dd3e98773d9ff5703c2a
dandua98 <danish@dandua.com>
2018-11-02 20:11:04 -0600

    basic command parsing setup

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import sqlite3
 import sys
 import os.path
 from authentication.member import Member
+from command.memberCommand import MemberCommand
 
 connection = None
 cursor = None
 
+mCmd = None
+
 user = None
 
 def connect(path):
-    global connection, cursor
+    global connection, cursor, mCmd
 
     connection = sqlite3.connect(path)
     cursor = connection.cursor()
     cursor.execute(' PRAGMA forteign_keys=ON; ')
     connection.create_function('HASH', 1, Member.hash)
     connection.commit()
+
+    mCmd = MemberCommand(cursor)
+
     return
 
 def main():
@@ -28,7 +34,7 @@ def main():
         print('ERROR: database file not found')
         sys.exit(0)
 
-    # member = command.user()
+    member = mCmd.user()
 
     connection.commit()
     connection.close()
