42b56ebd131095a1abe055d341b62feba8b306d5
dandua98 <danish@dandua.com>
2018-11-04 17:38:30 -0700

    offer ride finished

--- command/offerRideCommand.py
+++ command/offerRideCommand.py
@@ -1,6 +1,7 @@
 from command.command import Command
 from book_rides.offer_ride import OfferRide
 from datetime import datetime
+import re
 
 class OfferRideCommand(Command):
     def __init__(self, email, cursor):
@@ -15,7 +16,7 @@ class OfferRideCommand(Command):
         while not OfferRideCommand.validateDate(date):
             date = input("Invalid format, enter a date for this value (YYYY-MM-DD): ")
         dateArr = date.split("-")
-        date = datetime(dateArr[0], dateArr[1], dateArr[2])
+        date = datetime(int(dateArr[0]), int(dateArr[1]), int(dateArr[2]))
 
         nso = input("Enter the number of seats offered: ")
         while not nso.isdigit():
@@ -28,34 +29,35 @@ class OfferRideCommand(Command):
         lugDesc = input("Provide luggage description here (only first 10 chars accepted): ")
         lugDesc = lugDesc if len(lugDesc) <= 10 else lugDesc[0:10]
 
-        # TODO
         src = input("Provide a source location keyword: ")
-        locs = self.offer.findLocations(src)
+        locs = self.ofr.findLocations(src)
         while len(locs) == 0:
             src = input('No matching locations found! Enter another keyword: ')
-            locs = self.offer.findLocations(src)
-        src = self.offer.getLocationFrom(locs)
+            locs = self.ofr.findLocations(src)
+        src = self.ofr.getLocationFrom(locs)
 
         dst = input("Provide a destination location keyword: ")
-        locs = self.offer.findLocations(dst)
+        locs = self.ofr.findLocations(dst)
         while len(locs) == 0:
             dst = input('No matching locations found! Enter another keyword: ')
-            locs = self.offer.findLocations(dst)
-        src = self.offer.getLocationFrom(locs)
-        # DOTO
+            locs = self.ofr.findLocations(dst)
+        src = self.ofr.getLocationFrom(locs)
 
         cno = input("Do you want to add your car for this ride (y/n)?: ")
         while not cno.lower() in ['y', 'n']:
             cno = input("Invalid option, do you want to add your car for this ride (y/n)?: ")
-        while not self.ofr.checkIfCnoExists(cno):
-            cno = input("Car number provided not found! Enter 'n' to exit or provide a different number: ")
-            if cno.lower() == 'n':
-                break
+        if cno == 'y':
+            cno = input("Enter a car number to associate with this ride: ")
+            while not self.ofr.checkIfCnoExists(cno):
+                cno = input("Car number provided not found! Enter 'n' to exit or provide a different number: ")
+                if cno.lower() == 'n':
+                    cno = None
+                    break
 
         enroutes = []
         enr = 'y'
         while enr == 'y':
-            enr = input("Do you want to add an enroute location for this ride (y/n)?: ")
+            enr = input("Do you want to add an/another enroute location for this ride (y/n)?: ")
             if enr.lower() not in ['y', 'n']:
                 enr = 'y'
                 print('Invalid Option!')
@@ -63,7 +65,14 @@ class OfferRideCommand(Command):
             if enr == 'n':
                 break
             enrCode = input("Provide an enrote location keyword: ")
-        pass
+            locs = self.ofr.findLocations(enrCode)
+            while len(locs) == 0:
+                enrCode = input('No matching locations found! Enter another keyword: ')
+                locs = self.ofr.findLocations(enrCode)
+            enrCode = self.ofr.getLocationFrom(locs)
+            enroutes.append(enrCode)
+
+        self.ofr.newOffer(date, nso, ppc, lugDesc, src, dst, cno, enroutes)
 
     @staticmethod
     def validateDate(date):
--- command/searchrequestscommand.py
+++ command/searchrequestscommand.py
@@ -4,7 +4,7 @@ from search_requests.search_requests import SearchRequests
 class SearchRequestsCommand(Command)
     def __init__(self, cursor):
         super().__init__(cursor)
-    
+
     def menu(self):
         search_requests = SearchRequests(cursor, "darryl@oil.com")
         user_input = input("To search by pickup location, please enter a pickup location code. Otherwise, to view all requests, press Enter to continue.")
@@ -12,5 +12,5 @@ class SearchRequestsCommand(Command)
             search_requests.find_requests(user_input)
         else:
             search_requests.find_requests()
-        
+
         search_requests.display_results(0)
