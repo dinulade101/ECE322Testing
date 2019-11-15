88006ccfcb1c1038f0c04321759e8c5f858cfa21
Kai Bailey <kbailey1@ualberta.ca>
2019-11-15 14:18:14 -0700

    Setup coverage

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # 291-Mini-Project-I
 A command line application for ride sharing using SQLite as our database for CMPUT 291 Mini-Project-I
 
+## Testing
+
+To run all unittests
+```
+python3 -m unittest discover
+```
+
+To run a specific test suite
+```
+python3 -m unittest test.test_membercommand
+```
+
+To run the test coverage
+```
+pip3 install coverage
+coverage run -m unittest discover
+```
+
+To view the results and generate html page of results
+```
+coverage report -m 
+coverage html
+```
+
 ## Instructions
 To Install:
 ```
--- test/test_membercommand.py
+++ test/test_membercommand.py
@@ -2,13 +2,14 @@ import unittest
 from unittest.mock import patch
 from command import membercommand
 
+
 class MemberCommandTest(unittest.TestCase):
     @patch('sqlite3.Cursor')
     def testValidateEmail(self, mock_sql_cursor):
         memberCommand = membercommand.MemberCommand(mock_sql_cursor)
 
-        # Error guessing 
-        self.assertFalse(memberCommand.validateEmail("")) # empty input 
+        # Error guessing
+        self.assertFalse(memberCommand.validateEmail(""))  # empty input
 
         # Equivalence classes
         """
@@ -19,22 +20,28 @@ class MemberCommandTest(unittest.TestCase):
                 * string without '@'
                 * string containing special chars like *
         """
-        self.assertTrue(memberCommand.validateEmail("asdf@gmail.com")) # string including '@'
-        self.assertTrue(memberCommand.validateEmail("asdf123@g2mail.com")) # string of alpha numeric characters 
+        self.assertTrue(memberCommand.validateEmail(
+            "asdf@gmail.com"))  # string including '@'
+        # string of alpha numeric characters
+        self.assertTrue(memberCommand.validateEmail("asdf123@g2mail.com"))
 
-        self.assertFalse(memberCommand.validateEmail("asdf^gmail.com")) # string without '@'
-        self.assertFalse(memberCommand.validateEmail("asdf*@gmail.com")) # string containing special chars.
+        self.assertFalse(memberCommand.validateEmail(
+            "asdf^gmail.com"))  # string without '@'
+        # string containing special chars.
+        self.assertFalse(memberCommand.validateEmail("asdf*@gmail.com"))
 
     @patch('sqlite3.Cursor')
     def testValidateName(self, mock_sql_cursor):
         memberCommand = membercommand.MemberCommand(mock_sql_cursor)
 
         # Error guessing
-        self.assertFalse(memberCommand.validateName("")) # empty input
+        self.assertFalse(memberCommand.validateName(""))  # empty input
 
         # boundary testing where the charater limit is 20:
-        self.assertTrue(memberCommand.validateName("Asadnfjsdnfjsdnajfnf")) # 20 chars
-        self.assertFalse(memberCommand.validateName("Asadnfjsdnfjsdnajfnfn")) # 21 chars 
+        self.assertTrue(memberCommand.validateName(
+            "Asadnfjsdnfjsdnajfnf"))  # 20 chars
+        self.assertFalse(memberCommand.validateName(
+            "Asadnfjsdnfjsdnajfnfn"))  # 21 chars
 
         self.assertTrue(memberCommand.validateName("Bob"))
 
@@ -42,8 +49,8 @@ class MemberCommandTest(unittest.TestCase):
     def testValidatePhoneNumber(self, mock_sql_cursor):
         memberCommand = membercommand.MemberCommand(mock_sql_cursor)
 
-        # Error guessing 
-        self.assertFalse(memberCommand.validatePhone("")) # empty input 
+        # Error guessing
+        self.assertFalse(memberCommand.validatePhone(""))  # empty input
 
         # Equivalence classes
         """
@@ -55,15 +62,20 @@ class MemberCommandTest(unittest.TestCase):
                 * string having greater than 9 numbers 
                 * numbers not in format (NNN-NNN-NNNN)
         """
-        self.assertTrue(memberCommand.validatePhone("403-000-0000")) # 9 numeric characters in format (NNN-NNN-NNNN)
+        self.assertTrue(memberCommand.validatePhone("403-000-0000")
+                        )  # 9 numeric characters in format (NNN-NNN-NNNN)
 
-        self.assertFalse(memberCommand.validatePhone("40a-000-0000")) # non numeric characters
-        self.assertFalse(memberCommand.validatePhone("4-000-0000")) # less than 9 numbers
-        self.assertFalse(memberCommand.validatePhone("403-0001-0000")) # greater than 9 numbers
-        self.assertFalse(memberCommand.validatePhone("40-000-10000")) # numbers not in format 
+        self.assertFalse(memberCommand.validatePhone(
+            "40a-000-0000"))  # non numeric characters
+        self.assertFalse(memberCommand.validatePhone(
+            "4-000-0000"))  # less than 9 numbers
+        self.assertFalse(memberCommand.validatePhone(
+            "403-0001-0000"))  # greater than 9 numbers
+        self.assertFalse(memberCommand.validatePhone(
+            "40-000-10000"))  # numbers not in format
 
-        
-if __name__  == "main":
+
+if __name__ == "main":
     # with unittest.mock.patch('sys.argv', ['prj-tables.sql']):
     #     membercommand.main()
 
