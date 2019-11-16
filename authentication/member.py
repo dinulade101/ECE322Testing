ff327859d44d30c6cc662d6d4a4ebfe4317fbef9
Kai Bailey <kbailey1@ualberta.ca>
2019-11-16 12:38:31 -0700

    added integration tests for login and signup

--- authentication/member.py
+++ authentication/member.py
@@ -2,6 +2,7 @@ import hashlib
 import sys
 from messaging.message import Message
 
+
 class Member:
 
     def __init__(self, email, pwd, cursor):
@@ -12,25 +13,29 @@ class Member:
 
     @staticmethod
     def signup(email, name, phone, pwd, cursor):
-        cursor.execute("INSERT INTO members VALUES (?, ?, ?, ?)", (email, name, phone, pwd))
+        cursor.execute("INSERT INTO members VALUES (?, ?, ?, ?)",
+                       (email, name, phone, pwd))
         return Member(email, pwd, cursor)
 
     @staticmethod
     def checkIfExists(cursor, email):
-        cursor.execute("SELECT COUNT(*) FROM members WHERE email=:email", {"email":email})
+        cursor.execute(
+            "SELECT COUNT(*) FROM members WHERE email=:email", {"email": email})
         if cursor.fetchone()[0] != 0:
             return True
         return False
 
     def login(self, pwd):
-        self.cursor.execute("SELECT pwd FROM members WHERE email=:email", {"email":self.email})
+        self.cursor.execute("SELECT pwd FROM members WHERE email=:email", {
+                            "email": self.email})
         result = self.cursor.fetchone()
         if result == None or result[0] != pwd:
             return False
         return True
 
     def logout(self):
-        self.email = self.cursor = None
+        self.email = None
+        self.cursor = None
 
     def isLoggedIn(self):
         return self.email is not None
@@ -42,5 +47,6 @@ class Member:
         else:
             print("\nYou have no new messages!\n")
         for msg in msgs:
-            print("FROM: {0} | REGARDING: {1} | DATE: {2}\nCONTENT: {3}\n".format(msg[2], msg[4], msg[1], msg[3]))
+            print("FROM: {0} | REGARDING: {1} | DATE: {2}\nCONTENT: {3}\n".format(
+                msg[2], msg[4], msg[1], msg[3]))
         Message(self.cursor).markRead(self.email)
---
+++ test/test_authentication.py
@@ -0,0 +1,69 @@
+import unittest
+from unittest.mock import patch
+from unittest.mock import Mock
+from authentication.member import Member
+
+EMAIL = "test@test.com"
+PASSWORD = "password123"
+INVALID_EMAIL = 'invalid@invalid.com'
+INVALID_PASSWORD = "123"
+
+# Assumptions: The member class assumes that all data like email
+# phone, name and password that are passed to its functions are valid.
+# In order to validate the data the membercommand class can be used.
+
+
+class AuthenticationTest(unittest.TestCase):
+
+    # Unit Tests
+    def testCheckIfExists(self):
+        mock_sql_cursor = Mock()
+        mock_sql_cursor.fetchone.return_value = [1]
+        mem = Member(EMAIL, PASSWORD, mock_sql_cursor)
+
+        # Error guessing
+        self.assertTrue(mem.checkIfExists(mock_sql_cursor, EMAIL))
+
+        mock_sql_cursor.fetchone.return_value = [0]
+        self.assertFalse(mem.checkIfExists(mock_sql_cursor, INVALID_EMAIL))
+
+    def testSignup(self):
+        mock_sql_cursor = Mock()
+        mock_sql_cursor.fetchone.return_value = [PASSWORD]
+        mem = Member.signup(EMAIL, "jim", "111-111-1111",
+                            PASSWORD, mock_sql_cursor)
+        self.assertEqual(EMAIL, mem.email)
+
+    # Integration Tests
+
+    def testValidSignUpLogin(self):
+        # Signup a new user
+        mock_sql_cursor = Mock()
+        mock_sql_cursor.fetchone.return_value = [INVALID_PASSWORD]
+        mem = Member(EMAIL, PASSWORD, mock_sql_cursor)
+        self.assertFalse(mem.isLoggedIn())
+
+        # Login a new user
+        mock_sql_cursor.fetchone.return_value = [PASSWORD]
+        new_mem = mem.signup(EMAIL, "jim", "111-111-1111",
+                             PASSWORD, mock_sql_cursor)
+        self.assertTrue(new_mem.login(PASSWORD))
+        self.assertTrue(new_mem.isLoggedIn())
+        new_mem.logout()
+        self.assertFalse(new_mem.isLoggedIn())
+
+    def testInvalidSignUpLogin(self):
+        # Login a user that hasn't signed up
+        mock_sql_cursor = Mock()
+        mock_sql_cursor.fetchone.return_value = [INVALID_PASSWORD]
+        mem = Member(EMAIL, PASSWORD, mock_sql_cursor)
+        self.assertFalse(mem.isLoggedIn())
+
+        # Error found: Trying to login a user after initializing with an
+        # incorrect password throws an error instead of returning False
+        self.assertFalse(mem.login(PASSWORD))
+        self.assertFalse(mem.isLoggedIn())
+
+
+if __name__ == "main":
+    unittest.main()
